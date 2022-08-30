afc81f6825675df1b1aa21f6a11f96979ee57197
Rohit Singh Bisht <rohitsb485@gmail.com>
2022-08-30 10:52:51 +0530

    Added link component to team name in Row component

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Row = ({ team, data }) => {
+  const getTeamId = () => {
+    return team.toLowerCase().split(" ").join("-");
+  };
+
   return (
     <tr>
-      <td>{team}</td>
+      <td>
+        <Link to={`/teams/${getTeamId()}`}>{team}</Link>
+      </td>
       <td>{data[team].won}</td>
       <td>{data[team].lost}</td>
       <td>{data[team].draw}</td>
